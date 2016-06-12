4a3127a38671eac33810013895089d3ac3166871
James Netherton <jamesnetherton@gmail.com>
2016-06-12 10:17:15 +0100

    Fixes #4. Fix issue with STDIN content being ignored

--- src/main/java/com/github/jamesnetherton/lolcat4j/internal/Main.java
+++ src/main/java/com/github/jamesnetherton/lolcat4j/internal/Main.java
@@ -26,24 +26,22 @@
 package com.github.jamesnetherton.lolcat4j.internal;
 
 import com.github.jamesnetherton.lolcat4j.Lol;
-import com.github.jamesnetherton.lolcat4j.internal.commandline.OptionParseException;
 import com.github.jamesnetherton.lolcat4j.internal.commandline.OptionParser;
-import com.github.jamesnetherton.lolcat4j.internal.commandline.UnknownOptionException;
 import com.github.jamesnetherton.lolcat4j.internal.console.ConsoleReader;
 
-public final class LolCat {
+public final class Main {
 
     public static void main(String... args) {
         try {
             final Lol lol = new OptionParser().parse(args);
 
-            if (lol.getFiles().isEmpty()) {
+            if (lol.getFiles().isEmpty() && System.in.available() == 0) {
                 ConsoleReader consoleReader = new ConsoleReader(lol);
                 consoleReader.read();
             } else {
                 lol.cat();
             }
-        } catch (OptionParseException | UnknownOptionException e) {
+        } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(1);
         }
