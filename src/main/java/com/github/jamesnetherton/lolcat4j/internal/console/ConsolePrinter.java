1ce9bb0d81e2922a1fb9f9e3dc48ff2d0ade2114
James Netherton <jamesnetherton@gmail.com>
2026-03-21 09:53:38 +0000

    Build with minimum JDK 17

--- src/main/java/com/github/jamesnetherton/lolcat4j/internal/console/ConsolePrinter.java
+++ src/main/java/com/github/jamesnetherton/lolcat4j/internal/console/ConsolePrinter.java
@@ -1,69 +1,58 @@
-/*
- * #%L
- * lolcat4j
- * %%
- * Copyright (C) 2016 James Netherton
- * %%
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * #L%
- */
 package com.github.jamesnetherton.lolcat4j.internal.console;
 
 import com.github.jamesnetherton.lolcat4j.Lol;
 
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Locale;
 
 public class ConsolePrinter {
     private static final String ESCAPE_SEQUENCE_START = "\033[";
     private static final String ESCAPE_SEQUENCE_END = "\033[0m";
+    private static final boolean DEFAULT_ANSI_SUPPORTED = detectAnsiSupport();
+
     private final PrintStream printStream;
+    private final boolean ansiSupported;
 
     static {
-        // Make sure the console is restored to its original state if the application is interrupted
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                System.out.print(ESCAPE_SEQUENCE_START + "?25h");
-            }
-        });
+        if (DEFAULT_ANSI_SUPPORTED) {
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.print(ESCAPE_SEQUENCE_START + "?25h")));
+        }
     }
 
     public ConsolePrinter(PrintStream printStream) {
         this.printStream = printStream;
+        this.ansiSupported = DEFAULT_ANSI_SUPPORTED;
+    }
+
+    ConsolePrinter(PrintStream printStream, boolean ansiSupported) {
+        this.printStream = printStream;
+        this.ansiSupported = ansiSupported;
     }
 
     public void moveCursor(int amount) {
-        printStream.print(ESCAPE_SEQUENCE_START + amount + "D");
+        if (ansiSupported) {
+            printStream.print(ESCAPE_SEQUENCE_START + amount + "D");
+        }
     }
 
     public void showCursor() {
-        printStream.print(ESCAPE_SEQUENCE_START + "?25h");
+        if (ansiSupported) {
+            printStream.print(ESCAPE_SEQUENCE_START + "?25h");
+        }
     }
 
     public void hideCursor() {
-        printStream.print(ESCAPE_SEQUENCE_START + "?25l");
+        if (ansiSupported) {
+            printStream.print(ESCAPE_SEQUENCE_START + "?25l");
+        }
     }
 
     public void printColorized(Lol lol, int seed, int index, char charToPrint) {
-        printStream.print(ESCAPE_SEQUENCE_START + getHexRgbString(lol, seed, index) + "m" + charToPrint + ESCAPE_SEQUENCE_END);
+        if (ansiSupported) {
+            printStream.print(ESCAPE_SEQUENCE_START + getHexRgbString(lol, seed, index) + "m" + charToPrint + ESCAPE_SEQUENCE_END);
+        } else {
+            printStream.print(charToPrint);
+        }
     }
 
     public void printNewLine() {
@@ -71,13 +60,13 @@ public class ConsolePrinter {
     }
 
     public String cleanString(String s) {
-        return s.replaceAll("\\\\e\\[[\\d;]*[m|K]", "").replaceAll("\t", "        ");
+        return s.replaceAll("\\\\e\\[[\\d;]*[m|K]", "").replace("\t", "        ");
     }
 
     private String getHexRgbString(Lol lol, int seed, int index) {
         String rainbow = rainbow(lol.getFrequency(), seed + (index / lol.getSpread()));
-        List<Integer> rgbValues = hexToRgb(rainbow);
-        return rgb(rgbValues.get(0), rgbValues.get(1), rgbValues.get(2));
+        int[] rgb = hexToRgb(rainbow);
+        return rgb(rgb[0], rgb[1], rgb[2]);
     }
 
     private String rainbow(double frequency, double index) {
@@ -88,23 +77,9 @@ public class ConsolePrinter {
         return String.format("#%02X%02X%02X", (int) red, (int) green, (int) blue);
     }
 
-    private List<Integer> hexToRgb(String hexString) {
-        final String hexNoHash = hexString.replace("#", "");
-        final List<Integer> rgb = new ArrayList<>();
-        if (hexNoHash.length() == 6) {
-            int start = 0;
-            do {
-                String hex = hexNoHash.substring(start, start + 2);
-                rgb.add(Integer.parseInt(hex, 16));
-                start += 2;
-            } while (start <= hexNoHash.length() - 2);
-        } else {
-            for (int i = 0; i < hexNoHash.length(); i++) {
-                rgb.add(Integer.parseInt(String.valueOf(hexString.charAt(i))) * 2, 16);
-            }
-        }
-
-        return rgb;
+    private int[] hexToRgb(String hexString) {
+        int color = Integer.parseInt(hexString.substring(1), 16);
+        return new int[]{(color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF};
     }
 
     private String rgb(int red, int green, int blue) {
@@ -112,14 +87,13 @@ public class ConsolePrinter {
     }
 
     private String rgbValue(int red, int green, int blue) {
-        boolean grayPossible = true;
-        boolean gray = false;
         double sep = 42.5;
+        boolean gray = false;
 
-        while (grayPossible) {
+        while (true) {
             if (red < sep || green < sep || blue < sep) {
                 gray = red < sep && green < sep && blue < sep;
-                grayPossible = false;
+                break;
             }
             sep += 42.5;
         }
@@ -128,25 +102,22 @@ public class ConsolePrinter {
             int num = 232 + Math.round((float) red + (float) green + (float) blue / 33);
             return ";5;" + num;
         } else {
-            List<Integer> rgbValues = new ArrayList<>();
-            rgbValues.add(red);
-            rgbValues.add(green);
-            rgbValues.add(blue);
-
-            List<Integer> grayValues = new ArrayList<>();
-            grayValues.add(36);
-            grayValues.add(6);
-            grayValues.add(1);
-
-            int val = 0;
-
-            for (int i = 0; i < rgbValues.size(); i++) {
-                int color = rgbValues.get(i);
-                int mod = grayValues.get(i);
-                val += (int) (6 * ((float) color / 256)) * mod;
-            }
-
+            int val = (int) (6 * ((float) red / 256)) * 36
+                    + (int) (6 * ((float) green / 256)) * 6
+                    + (int) (6 * ((float) blue / 256));
             return ";5;" + (val + 16);
         }
+    }
+
+    private static boolean detectAnsiSupport() {
+        String os = System.getProperty("os.name", "").toLowerCase(Locale.ROOT);
+        if (os.contains("windows")) {
+            // Windows Terminal sets WT_SESSION; ConEmu sets ConEmuANSI; Git Bash / MSYS2 set TERM_PROGRAM
+            return System.getenv("WT_SESSION") != null
+                || "ON".equalsIgnoreCase(System.getenv("ConEmuANSI"))
+                || System.getenv("TERM_PROGRAM") != null;
+        }
+        String term = System.getenv("TERM");
+        return term != null && !term.equals("dumb");
     }
 }
